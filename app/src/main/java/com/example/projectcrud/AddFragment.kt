cd446f0bd55fb943ec4f5b2885b1c442040f8d39
Adri Encina <adr.encina@gmail.com>
2024-05-28 00:16:38 -0300

    cambios sin api.

--- app/src/main/java/com/example/projectcrud/AddFragment.kt
+++ app/src/main/java/com/example/projectcrud/AddFragment.kt
@@ -51,7 +51,6 @@ class AddFragment : Fragment() {
 
 
 
-//// TODO: Rename parameter arguments, choose names that match
 //// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 //private const val ARG_PARAM1 = "param1"
 //private const val ARG_PARAM2 = "param2"
@@ -62,7 +61,6 @@ class AddFragment : Fragment() {
 // * create an instance of this fragment.
 // */
 //class AddFragment : Fragment() {
-//    // TODO: Rename and change types of parameters
 //    private var param1: String? = null
 //    private var param2: String? = null
 //
@@ -91,7 +89,6 @@ class AddFragment : Fragment() {
 //         * @param param2 Parameter 2.
 //         * @return A new instance of fragment AddFragment.
 //         */
-//        // TODO: Rename and change types and number of parameters
 //        @JvmStatic
 //        fun newInstance(param1: String, param2: String) =
 //            AddFragment().apply {
--- app/src/main/java/com/example/projectcrud/DeleteFragment.kt
+++ app/src/main/java/com/example/projectcrud/DeleteFragment.kt
@@ -44,7 +44,6 @@ class DeleteFragment : Fragment() {
 
 
 //
-//// TODO: Rename parameter arguments, choose names that match
 //// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 //private const val ARG_PARAM1 = "param1"
 //private const val ARG_PARAM2 = "param2"
@@ -55,7 +54,6 @@ class DeleteFragment : Fragment() {
 // * create an instance of this fragment.
 // */
 //class DeleteFragment : Fragment() {
-//    // TODO: Rename and change types of parameters
 //    private var param1: String? = null
 //    private var param2: String? = null
 //
