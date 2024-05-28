fc54391de1d4cf9a5f13a8ca656a26407f0fe86c
Adri Encina <adr.encina@gmail.com>
2024-05-28 00:53:52 -0300

    cambios sin api. No crash!!

--- app/src/main/java/com/example/projectcrud/MainActivity.kt
+++ app/src/main/java/com/example/projectcrud/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.projectcrud
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -12,7 +10,6 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.projectcrud.databinding.ActivityMainBinding
-import com.example.projectcrud.ui.add.AddFragment
 
 class MainActivity : AppCompatActivity() {
 
